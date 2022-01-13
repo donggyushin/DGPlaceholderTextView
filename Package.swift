d190fdba8256dca191c071d2b1e20eb99ba78378
신동규 <donggyu9410@gmail.com>
2022-01-14 02:14:24 +0900

    Create DGPlaceholderTextView

--- Package.swift
+++ Package.swift
@@ -5,19 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "DGPlaceholderTextView",
+    platforms: [
+            .iOS(.v12)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DGPlaceholderTextView",
             targets: ["DGPlaceholderTextView"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DGPlaceholderTextView",
             dependencies: []),
--- Sources/DGPlaceholderTextView/DGPlaceholderTextView.swift
+++ Sources/DGPlaceholderTextView/DGPlaceholderTextView.swift
@@ -1,6 +1,56 @@
-public struct DGPlaceholderTextView {
-    public private(set) var text = "Hello, World!"
+import UIKit
 
-    public init() {
+public class DGPlaceholderTextView: UITextView {
+    
+    public var horizontalPadding: CGFloat = 4.5
+    public var verticalPadding: CGFloat = 7.5
+    
+    public override var font: UIFont? {
+        didSet {
+            placeholderLabel.font = font
+        }
+    }
+    
+    public var placeholder: String? {
+        didSet {
+            placeholderLabel.text = placeholder
+        }
+    }
+    
+    public var placeholderTextColor: UIColor? {
+        didSet {
+            placeholderLabel.textColor = placeholderTextColor
+        }
+    }
+    
+    private lazy var placeholderLabel: UILabel = {
+        let view = UILabel()
+        view.numberOfLines = 0
+        view.textColor = .gray
+        return view
+    }()
+    
+    public override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        configureUI()
+        delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        addSubview(placeholderLabel)
+        placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
+        placeholderLabel.topAnchor.constraint(equalTo: topAnchor, constant: verticalPadding).isActive = true
+        placeholderLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: horizontalPadding).isActive = true
+        placeholderLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -horizontalPadding).isActive = true
+    }
+}
+
+extension DGPlaceholderTextView: UITextViewDelegate {
+    public func textViewDidChange(_ textView: UITextView) {
+        self.placeholderLabel.isHidden = !textView.text.isEmpty
     }
 }
